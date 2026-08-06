599ad8f242765d92cf6995bb45e1e3b94290b7a5
Colin Watson <137962378+ceeewatt@users.noreply.github.com>
2026-08-06 14:21:45 +0000

    overhaul bash config
    This *should* be compatible for both Fedora and Ubuntu.

--- dot_bashrc.d/aliases.sh
+++
@@ -1,8 +0,0 @@
-alias la='ls -lhAF --color=auto'
-alias j='jobs'
-
-alias man-search='man -k . | fzf --height 40% | awk "{print \$1}" | xargs -r man'
-
-mkcd() {
-    mkdir -p -- "$1" && cd -P -- "$1"
-}
