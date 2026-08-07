f0339544280ae5de7307ea6f04cc72227e16afa8
Colin Watson <137962378+ceeewatt@users.noreply.github.com>
2026-08-07 13:31:13 +0000

    add JetBrainsMono nerd font install script

---
+++ .chezmoiscripts/run_onchange_04-install-nerd-font.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+ver=''   # eg: v3.5.0
+
+asset='JetBrainsMono.tar.xz'
+root='https://github.com/ryanoasis/nerd-fonts'
+
+if [ -z "$ver" ]; then
+    url="${root}/releases/latest/download/${asset}"
+else
+    url="${root}/releases/download/${ver}/${asset}"
+fi
+
+echo Downloading nerd font $asset from $url
+
+mkdir -p ~/.local/share/fonts/JetBrainsMonoNerdFont
+rm -f ~/.local/share/fonts/JetBrainsMonoNerdFont/*
+curl -fsSL $url | tar -xJf - -C ~/.local/share/fonts/JetBrainsMonoNerdFont
+
+fc-cache -f
